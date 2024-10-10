cf531184f126939e9aa8cc12c4ee1be92f1d3688
Tarcyo <55460663+Tarcyo@users.noreply.github.com>
2024-10-10 11:47:08 -0300

    Add files via upload

--- lib/reusableWidgets/insertCamp.dart
+++ lib/reusableWidgets/insertCamp.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart'; 
+
 class RoundedTextField extends StatelessWidget {
   final TextEditingController controller;
   final List<TextInputFormatter> inputFormatters; 
@@ -14,7 +15,7 @@ class RoundedTextField extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 500, 
-      height: 20, 
+      height: 20, // Ajustei a altura para melhor espaçamento vertical
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(20.0),
@@ -30,15 +31,15 @@ class RoundedTextField extends StatelessWidget {
       child: Center(
         child: TextField(
           controller: controller,
-          textAlignVertical: TextAlignVertical.center, 
-          textAlign: TextAlign.start, 
+          textAlignVertical: TextAlignVertical.center, // Centraliza verticalmente
           decoration: InputDecoration(
             border: InputBorder.none,
             hintText: '', 
+            contentPadding: EdgeInsets.only(left: 5, top: 10, bottom: 10), // Espaçamento da borda esquerda
           ),
           keyboardType: TextInputType.multiline,
           inputFormatters: inputFormatters, 
-          style: TextStyle(fontSize: 14, color: Colors.grey[900]),
+          style: TextStyle(fontSize: 13, color: Colors.grey[900]),
         ),
       ),
     );
